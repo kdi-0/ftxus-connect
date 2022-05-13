02e360555248559b7596dc18f168eb30528c7f31
kdi <kerwin.dahomey@gmail.com>
2022-05-12 22:21:59 -0500

    begin adding websocketpp

---
+++ out/build/CMakeFiles/silly-ray.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/silly-ray.dir/adder.cpp.o"
+  "CMakeFiles/silly-ray.dir/adder.cpp.o.d"
+  "CMakeFiles/silly-ray.dir/main.cpp.o"
+  "CMakeFiles/silly-ray.dir/main.cpp.o.d"
+  "silly-ray"
+  "silly-ray.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/silly-ray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
